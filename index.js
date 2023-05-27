fca740cc2f7ab3591f761128a5db95397af7027d
Farabi Rifat <ifatkhan633@gmail.com>
2023-05-27 17:44:48 +0600

    user registration & login api created, jwt token used, user pasword encrypted

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const cors = require('cors');
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const User = require('./models/User')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
 const app = express();
 
+// password incription -----------------------------------
+const bcryptSalt = bcrypt.genSaltSync(10);
+// jwt token incription --------------------------------
+const jwtSecret = 'sdjhdgsfuysgfiwewfh';
 
-const bcryptSalt = bcrypt.genSaltSync(10)
+
+app.use(express.json());
+app.use(cookieParser())
 
 
-app.use(express.json())
-
-// H8tQ4qTgog9EbABG
 
 app.use(cors({ 
     credentials:true,
@@ -27,6 +32,10 @@ app.get('/test', (req,res)=>{
    
 });
 
+
+// register user 1-----------------------------------
+// ---------------------------------
+
 app.post('/register', async (req,res) =>{
     const {name, email, password} = req.body;
 
@@ -40,6 +49,51 @@ app.post('/register', async (req,res) =>{
     } catch(e){
         res.status(422).json(e)
     }
+    })
+
+// login user 2----------------------------------------
+// -----------------------------------------
+
+    app.post('/login',async (req,res)=>{
+        const {email,password}= req.body;
+        const userDoc= await User.findOne({email});
+        if(userDoc){
+           const passOk = bcrypt.compareSync(password, userDoc.password)
+           if(passOk){
+            jwt.sign({
+                email:userDoc.email, 
+                id:userDoc._id,
+            }, jwtSecret, {}, (err, token)=>{
+                if(err) throw err;
+                res.cookie('token', token).json(userDoc);
+            })
+           }
+           else{
+            res.status(422).json('not ok')
+           }
+        }
+        else{
+            res.json('not')
+        }
+    })
+
+
+    // user profile 3---------------------------------------------------
+    // --------------------------------------
+
+    app.get('/profile',  (req, res) =>{
+        const {token} = req.cookies;
+        if(token){
+            jwt.verify(token, jwtSecret, {},async (err, userData)=>{
+                if(err) throw err;
+                const {name,email, _id} = await User.findById(userData.id)
+                res.json({name, email, _id});
+            })
+        }
+        else{
+            res.json(null);
+        }
+        
     })
 
 app.listen(4000, () => {
